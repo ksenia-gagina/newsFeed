0d94e8933ed5e547c5642bddb0279ad671c77e1f
Ksenua <Gagina>
2025-03-20 22:45:26 +0300

    initial project

---
+++ newsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsFeed
+//
+//  Created by Ксения Гагина on 20.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
